1df01f7fea5c16a031b0f23f2d13cb60ba4cf7e4
guimoreirapereira <guimoreirapereira@gmail.com>
2022-07-04 22:30:44 -0300

    Adiciona funcionalidade de criar tarefas

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,4 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, constr
+from uuid import UUID, uuid4
+from enum import Enum
+from fastapi import FastAPI, status
+
+class EstadosPossiveis(str, Enum):
+    finalizado = "finalizado"
+    nao_finalizado = "não finalizado"
+
+
+class TarefaEntrada(BaseModel):
+    titulo: constr(min_length=3, max_length=50)
+    descricao: constr(max_length=140)
+    estado: EstadosPossiveis = EstadosPossiveis.nao_finalizado
+
+class Tarefa(TarefaEntrada):
+    id: UUID
+
 
 TAREFAS = [
     {
@@ -28,3 +46,12 @@ app = FastAPI()
 @app.get("/tarefas")
 def listar():
     return TAREFAS
+
+
+@app.post('/tarefas', response_model=Tarefa, status_code=status.HTTP_201_CREATED)
+def criar(tarefa: TarefaEntrada):
+    nova_tarefa = tarefa.dict()
+    nova_tarefa.update({"id": uuid4()})
+    TAREFAS.append(nova_tarefa)
+    return nova_tarefa
+
